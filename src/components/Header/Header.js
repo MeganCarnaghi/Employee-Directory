b4fc8adfc6b18d848063eae321ef1ecdce4e6bf5
Megan <carnagh5@gmail.com>
2021-01-26 20:07:33 -0500

    updated header, app.js and mainfiest.json

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+// Header with use instructions
+function Header() {
+  return (
+    <div className="header">
+      <h1>Employee Directory</h1>
+      <p>Directions On How To Use This Directory</p>
+    </div>
+  );
+}
+
+export default Header;
